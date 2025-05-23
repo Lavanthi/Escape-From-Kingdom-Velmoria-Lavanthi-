40053027e754e017389c442414088df165113f38
Lavanthi <80131633+Lavanthi@users.noreply.github.com>
2025-05-22 22:15:43 -0400

    Create NeededItem.java

---
+++ NeededItem.java
@@ -0,0 +1,18 @@
+/*
+This class extends from the item class. It was made for only the items needed to give to the witch.
+AKA the spellbook, wand and purple diamond.
+ */
+public class NeededItem extends Item{
+    //will be used in the end of the same
+    private boolean isGivenToWitch;
+
+    //constructor
+    public NeededItem(String name,String description){
+        super(name,description);
+        isGivenToWitch=false;
+    }
+
+    //methods are made so that if the player wants to win the game, they can only win if these items are given to the witch
+    public boolean isGivenToWitch(){return isGivenToWitch;}
+    public void setGivenToWitch(boolean given) {isGivenToWitch = given;}
+}
